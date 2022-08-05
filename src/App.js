8bf7db6ea92fe1f3b88be6d780f10593ae656fa5
qiubao-code <1853413171@qq.com>
2022-08-05 20:04:35 +0800

    home page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,112 @@
 import React from "react";
+import shapeTop from "./images/shape-1.png"
+import shapeBottom from "./images/shape-2.png"
+import QusitionList from "./components/QusitionList"
+import { Breadcrumb, Layout, Menu, Button, Dropdown, message, Space, Tooltip } from "antd"
+import { DownOutlined, UserOutlined } from '@ant-design/icons';
+import data from "./data"
 
-export default function App(){
-    return 123
+
+export default function App() {
+    const { Header, Content, Footer } = Layout;
+    const [category,setCategory] = React.useState({data});
+    console.log(category);
+
+    const handleButtonClick = (e) => {
+        message.info('Click on left button.');
+        console.log('click left button', e);
+    };
+
+    const handleMenuClick = (e) => {
+        message.info('Click on menu item.');
+        console.log('click', e);
+    };
+    function setMenu(){
+        for(let i = 0;i<category.data.length;i++){
+            return{
+                label:category.data[i].value,
+                key:category.data[i].id
+            }
+        }
+    }
+
+    const menu = (
+        <Menu
+            onClick={handleMenuClick}
+            items={data.map(item=>({label:item.value,key:item.id}))}
+            // items={[
+            //     {
+            //         label: 'Any Category',
+            //         key: '1',
+            //     },
+            //     {
+            //         label: '2nd menu item',
+            //         key: '2',
+            //     },
+            //     {
+            //         label: '3rd menu item',
+            //         key: '3',
+            //     },
+            // ]}
+        />
+    );
+    return (
+        <Layout className="layout">
+            <Header>
+                <div className="logo">
+
+                </div>
+                <Menu
+                    theme="dark"
+                    mode="horizontal"
+                    defaultSelectedKeys={['2']}
+                    items={new Array(3).fill(null).map((_, index) => {
+                        const key = index + 1;
+                        return {
+                            key,
+                            label: `nav${key}`
+                        }
+                    })}
+                >
+
+                </Menu>
+            </Header>
+            <Content style={{ padding: '0 50px', }}>
+                <Breadcrumb style={{ margin: '16px 0', }}>
+                    <Breadcrumb.Item>Home</Breadcrumb.Item>
+                    <Breadcrumb.Item>List</Breadcrumb.Item>
+                    <Breadcrumb.Item>App</Breadcrumb.Item>
+                </Breadcrumb>
+
+                <div className="site-layout-content">
+                    <img className="shape-top" src={shapeTop} alt="shape-top" />
+
+                    <div className="game-intro">
+                        <h1 className="game-title">Quizzical</h1>
+                        <p className="game-text">Answer the questions and test your knowledge!</p>
+                    </div>
+
+                    <Dropdown overlay={menu} trigger={['click']} >
+                        <Button>
+                            <Space>
+                                Button
+                                <DownOutlined />
+                            </Space>
+                        </Button>
+                    </Dropdown>
+
+                    <QusitionList />
+                    <img className="shape-bottom" src={shapeBottom} alt="shape-bottom" />
+                </div>
+
+            </Content>
+            <Footer
+                style={{
+                    textAlign: 'center',
+                }}
+            >
+                quizzical ©2022 Created by Qiu
+            </Footer>
+        </Layout >
+    )
 }
---
+++ src/data.js
@@ -0,0 +1,103 @@
+const category = [
+    {
+        id: 1,
+        value: "Any Category"
+    },
+    {
+        id: 2,
+        value: "General Knowledge"
+    },
+    {
+        id: 3,
+        value: "Entertainment: Books"
+    },
+    {
+        id: 4,
+        value: "Entertainment: Film"
+    },
+    {
+        id: 5,
+        value: "Entertainment: Music"
+    },
+    {
+        id: 6,
+        value: "Entertainment: Musicals & Theatres"
+    },
+    {
+        id: 7,
+        value: "Entertainment: Television"
+    },
+    {
+        id: 8,
+        value: "Entertainment: Video Games"
+    },
+    {
+        id: 9,
+        value: "Entertainment: Board Games"
+    },
+    {
+        id: 10,
+        value: "Science & Nature"
+    },
+    {
+        id: 11,
+        value: "Science: Computers"
+    },
+    {
+        id: 12,
+        value: "Science: Mathematics"
+    },
+    {
+        id: 13,
+        value: "Mythology"
+    },
+    {
+        id: 14,
+        value: "Sports"
+    },
+    {
+        id: 15,
+        value: "Geography"
+    },
+    {
+        id: 16,
+        value: "History"
+    },
+    {
+        id: 17,
+        value: "Politics"
+    },
+    {
+        id: 18,
+        value: "Art"
+    },
+    {
+        id: 19,
+        value: "Celebrities"
+    },
+    {
+        id: 20,
+        value: "Animals"
+    },
+    {
+        id: 21,
+        value: "Vehicles"
+    },
+    {
+        id: 22,
+        value: "Entertainment: Comics"
+    },
+    {
+        id: 23,
+        value: "Science: Gadgets"
+    },
+    {
+        id: 24,
+        value: "Entertainment: Japanese Anime & Manga"
+    },
+    {
+        id: 25,
+        value: "Entertainment: Cartoon & Animations"
+    }
+]
+export default category;
